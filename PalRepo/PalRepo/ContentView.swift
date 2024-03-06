eeaad03f6a50503a48031ed90b6492cd1265cbec
adin-apple <157755690+adin-apple@users.noreply.github.com>
2024-03-05 20:31:42 -0500

    Created Project
    XCode project created.

---
+++ PalRepo/PalRepo/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  PalRepo
+//
+//  Created by Adin Donlagic on 3/5/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PalRepo/PalRepo/PalRepoApp.swift
@@ -0,0 +1,18 @@
+//
+//  PalRepoApp.swift
+//  PalRepo
+//
+//  Created by Adin Donlagic on 3/5/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PalRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
